4b6f4a423ec8375c65f83b31f8ff29f79071a653
gaeun <ge97@yonsei.ac.kr>
2019-10-28 01:50:38 +0900

    no message

--- frame_capture.py
+++ frame_capture.py
@@ -1,22 +1,35 @@
 import cv2
+import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-dp', '--dirpath', type=str, required=True, help="Directory path")
-parser.add_argument('-cl', '--cutlength', type=int, required=True, help="Clip length")
+parser.add_argument('-dp', '--dirpath', type=str, required=True, help="Directory paht)
 args = parser.parse_args()
 
+dir_path = args.dirpath
 
+string = os.popen('ls' + dir_path + ' | grep.mp4').read()
 
+mp4_list = string.split("\n")
 
-vidcap = cv2.VideoCapture('twice3-1.mp4')
-success,image = vidcap.read()
-count = 0
-success = True
+video_list = []
+                    
+for v in mp4_list:
+    if v[-4:] == ".mp4":
+        video_list.append(v)
 
-while success:
-  cv2.imwrite("test/frame%d.jpg" % count, image)     # save frame as JPEG file
+print(video_list)
+
+for i in range(0, len(video_list))                  
+  vidcap = cv2.VideoCapture(video_list[i])
   success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
-
+  count = 0
+  success = True
+  while success:
+    success,image = vidcap.read()
+    if(count%9==0):
+        imageName = name[:-4] + str(count) + '.jpg'
+        cv2.imwrite("%s%d.jpg" % video_list[i] % count, image)     # save frame as JPEG file
+    if cv2.waitKey(10) == 27:                     # exit if Escape is hit
+        break
+    count += 1
